d0a061bc86ad81a751937c8e09660e12e333d41a
iestyn129 <iestyn129msm@gmail.com>
2026-06-24 10:34:00 +0100

    commit 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,34 @@
-#include "hk/gfx/DebugRenderer.h"
+#include "cstdio"
 #include "hk/hook/Trampoline.h"
 #include "hk/svc/api.h"
 
-#include "nn/fs.h"
-
-HkTrampoline<void, void*> sdFileCreate = hk::hook::trampoline([](void* a1) -> void {
-    sdFileCreate.orig(a1);
-    nn::fs::MountSdCard("sd");
-
-    nn::fs::CreateFile("sd://Hi_there.txt", 0);
-
+// Runs on startup
+HkTrampoline<void, u64*> initHook = hk::hook::trampoline([](u64* a1) -> void {
+    initHook.orig(a1);
 });
 
-HkTrampoline<int, void*, int> LMS_GetTextHook = hk::hook::trampoline([](void* a1, int a2) -> int {
-    return LMS_GetTextHook.orig(a1, 5);
-});
 
 // Runs every frame
-HkTrampoline<void, int> testHook = hk::hook::trampoline([](int a1) -> void {
-    testHook.orig(a1);
-    hk::svc::OutputDebugString("Hi!\n", 5);
+HkTrampoline<void, int> mainLoopHook = hk::hook::trampoline([](int a1) -> void {
+    mainLoopHook.orig(a1);
 });
 
-extern "C" void hkMain() {
-    testHook.installAtMainOffset(0x509e10);
-    sdFileCreate.installAtOffset(hk::ro::getMainModule(), 0x50c9c0);
-    LMS_GetTextHook.installAtMainOffset(0x4f0ba0);
 
-   // hk::gfx::DebugRenderer::instance()->installHooks();
+HkTrampoline<void*, char*, u32*, u32, s32, s32, char, char> loadFileHook = hk::hook::trampoline([](
+    char* a1, unsigned int* a2, unsigned int a3, int a4, int a5, char a6, char a7
+) -> void* {
+    void* ret = loadFileHook.orig(a1, a2, a3, a4, a5, a6, a7);
+
+    char msg[1024];
+    sprintf(msg, "loaded file '%s' at %p. (a2: %p, a3: %d, a4: %d, a5: %d, a6: %d, read only?: %d)", a1, ret, a2, a3, a4, a5, a6, a7);
+    hk::svc::OutputDebugString(msg, 1024);
+
+    return ret;
+});
+
+
+extern "C" void hkMain() {
+    initHook.installAtMainOffset(0x50C9C0);
+    mainLoopHook.installAtMainOffset(0x509e10);
+    loadFileHook.installAtMainOffset(0x4E3990);
 }
